3a94505b7a8f8a7c99959770d9b7eff2e2611fb8
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-06-10 17:50:00 +0200

    Add type for sing in input

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -52,7 +52,7 @@ export class SignIn extends React.Component<{}, IState> {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               Sign In With Google
             </CustomButton>
           </div>
