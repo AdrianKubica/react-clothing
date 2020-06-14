a4b50d633e7c007b4696bc86fc907c0b5a49e87f
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-06-14 13:01:05 +0200

    Redirect user to home if user is signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import "./App.scss"
 import { HomePage } from "./pages/homepage/homepage.component"
 import { ShopPage } from "./pages/shop/shop.component"
@@ -12,7 +12,7 @@ import { setCurrentUser } from "./store/user/user.actions"
 import { User, SetCurrentUserAction } from "./store/user/user.types"
 import { Dispatch } from "redux"
 
-class App extends React.Component {
+class App extends React.Component<PropsFromRedux> {
   unsubscribeFromAuth!: Unsubscribe
 
   componentDidMount() {
@@ -42,20 +42,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/auth" component={AuthPage} />
+          <Route exact path="/auth" render={() => (this.props.currentUser ? <Redirect to="/" /> : <AuthPage />)} />
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({ user }: { user: User }) => ({
+  currentUser: user.currentUser,
+})
+
 const mapDispatchToProps = (dispatch: Dispatch<SetCurrentUserAction>) => ({
   setCurrentUser: (user: User) => dispatch(setCurrentUser(user)),
 })
 
-// export default connect(null, mapDispatchToProps)(App)
-
-const connector = connect(null, mapDispatchToProps)
+const connector = connect(mapStateToProps, mapDispatchToProps)
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
