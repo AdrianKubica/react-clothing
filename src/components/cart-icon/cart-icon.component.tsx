e438e94900fc48cf44b6be9da913f03fbb71adc8
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-06-20 22:48:11 +0200

    Dropdown with selector hooks useSelector and useDispatch

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,25 +1,23 @@
 import React from "react"
 import "./cart-icon.styles.scss"
 import { ReactComponent as ShoppingIcon } from "../../assets/img/shopping-bag.svg"
-import { toggleCartVisibility } from "../../store/cart/cart.actions"
-import { connect, ConnectedProps } from "react-redux"
-import { Store } from "redux"
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
 import { StoreState } from "../../store/root.reducer"
+import { TOGGLE_HIDDEN } from "../../store/cart/cart.types"
 
-const CartIcon = ({ itemCount, toggleCartVisibility }: PropsFromRedux) => (
-  <div className="cart-icon" onClick={toggleCartVisibility}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
-)
-
-const mapStateToProps = ({ cart: { cartItems } }: StoreState) => {
-  return {
-    itemCount: cartItems.reduce((prev, next) => prev + next.quantity!, 0),
-  }
+const reduce = ({ cart: {cartItems} }: StoreState) => {
+  return cartItems.reduce((prev, next) => prev + next.quantity!, 0)
 }
 
-const connector = connect(mapStateToProps, { toggleCartVisibility })
-type PropsFromRedux = ConnectedProps<typeof connector>
+const CartIcon = () => {
+  const itemCount = useSelector(reduce, shallowEqual)
+  const dispatch = useDispatch()
+  return (
+    <div className="cart-icon" onClick={() => dispatch({ type: TOGGLE_HIDDEN })}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  )
+}
 
-export default connector(CartIcon)
+export default CartIcon
