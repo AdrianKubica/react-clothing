9075912de8d62354c62f33dd2e3d16e77cd8c517
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-06-22 00:16:08 +0200

    Redux-logger only for development build

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,7 +4,11 @@ import { applyMiddleware, createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
 import { persistStore } from "redux-persist"
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
 
